e1224d12701ea48cab4f260a0eee012b903400fe
Claudia Huertas <clauhuertas@gmail.com>
2021-04-23 18:02:19 +0200

    Update db_stock

--- R/db_stock.R
+++ R/db_stock.R
@@ -3,8 +3,9 @@ year_p16=2015
 unit="square" ###"habitat", "square"
 sq=250
 p16=TRUE
-palms=TRUE
+palms=FALSE
 write=TRUE
+
 
 # db_stock(year=2015,year_p16=2015,unit="square",sq=250,p16=TRUE,palms=TRUE,write=TRUE)
 
@@ -39,12 +40,15 @@ db_stock<-function(year,year_p16,unit,sq,p16,palms,write,grilla){
   
   # Include or not palms
   if (palms==TRUE) {
-    db=db
+    db2=db
   } else if (palms==FALSE) {
-    db=db[which(db$family!="Arecaceae"),]
+    db2=db[which(db$family!="Arecaceae"),]
   } else {
     stop("Error in stating whether palms are included or not")
   }
+  
+  
+
   
   ## Reassigned the unit or analysis square
   if(unit=="square"){
@@ -53,13 +57,13 @@ db_stock<-function(year,year_p16,unit,sq,p16,palms,write,grilla){
     crs(grid) <- "+init=epsg:32622"
     grid=as.data.frame(grid)
     if (sq==62|sq==62.5) {
-      db$square=db$square_62
+      db2$square=db2$square_62
       grid_area=aggregate(list(area=grid[,c("area")]),by = list(square=grid$square_62,trait=grid$trait,plot=grid$parcelle),FUN = sum)
     } else if (sq==125) {
-      db$square=db$square_125
+      db2$square=db2$square_125
       grid_area=aggregate(list(area=grid[,c("area")]),by = list(square=grid$square_125,trait=grid$trait,plot=grid$parcelle),FUN = sum)
     } else if (sq==250) {
-      db$square=db$square_250
+      db2$square=db2$square_250
       grid_area=aggregate(list(area=grid[,c("area")]),by = list(square=grid$square_250,trait=grid$trait,plot=grid$parcelle),FUN = sum)
     }else{
       stop("Error defining the square")
@@ -72,15 +76,15 @@ db_stock<-function(year,year_p16,unit,sq,p16,palms,write,grilla){
     crs(ferry) <- "+init=epsg:32622"
     ferry=as.data.frame(ferry)
     if (sq==62|sq==62.5) {
-      db$square=paste0(db$square_62,"-",db$habitat)
+      db2$square=paste0(db2$square_62,"-",db2$habitat)
       ferry_area=aggregate(list(area=ferry[,c("area")]),by = list(square=paste0(ferry$square_62,"-",habitat=ferry$habitats),trait=ferry$trait,
                                                                   plot=ferry$parcelle),FUN = sum)
     } else if (sq==125) {
-      db$square=paste0(db$square_125,"-",db$habitat)
+      db2$square=paste0(db2$square_125,"-",db2$habitat)
       ferry_area=aggregate(list(area=ferry[,c("area")]),by = list(square=paste0(ferry$square_125,"-",habitat=ferry$habitats),trait=ferry$trait,
                                                                   plot=ferry$parcelle),FUN = sum)
     } else if (sq==250) {
-      db$square=paste0(db$square_250,"-",db$habitat)
+      db2$square=paste0(db2$square_250,"-",db2$habitat)
       ferry_area=aggregate(list(area=ferry[,c("area")]),by = list(square=paste0(ferry$square_250,"-",habitat=ferry$habitats),trait=ferry$trait,
                                                                   plot=ferry$parcelle),FUN = sum)
       
@@ -94,28 +98,28 @@ db_stock<-function(year,year_p16,unit,sq,p16,palms,write,grilla){
   
   
   # Aggregation of stock variables
-  db_sum=aggregate(db[,c("ba","agb","agv")], by=list(square=db$square), sum)
-  db_len=aggregate(list(n=db[,c("idtree")]), by=list(square=db$square), length)
-  db_mean=aggregate(db[,c("dbh","wd")], by=list(square=db$square), mean)
+  db2_sum=aggregate(db2[,c("ba","agb","agv")], by=list(square=db2$square), sum)
+  db2_len=aggregate(list(n=db2[,c("idtree")]), by=list(square=db2$square), length)
+  db2_mean=aggregate(db2[,c("dbh","wd")], by=list(square=db2$square), mean)
   
   if (unit=="habitat") {
-    db<-as.data.frame(Reduce(function(...) merge(..., all = TRUE, by ="square"),list(db_sum,db_len,db_mean,ferry_area)))
+    db2<-as.data.frame(Reduce(function(...) merge(..., all = TRUE, by ="square"),list(db2_sum,db2_len,db2_mean,ferry_area)))
   } else if (unit=="square") {
-    db<-as.data.frame(Reduce(function(...) merge(..., all = TRUE, by ="square"),list(db_sum,db_len,db_mean,grid_area)))
+    db2<-as.data.frame(Reduce(function(...) merge(..., all = TRUE, by ="square"),list(db2_sum,db2_len,db2_mean,grid_area)))
   } else {
     stop("Error defining the study (habitat or square) unit")
   }
   
   # Eliminate those individuals that are outside of the grids
-  db=db[which(!is.na(db$area)),]
-  db$year=year
-  db$unit=unit
-  db$sq=sq
-  db$p16=p16
-  db$palms=palms
-  db$norm=FALSE # If the ground variables were normalized by area
+  db2=db2[which(!is.na(db2$area)),]
+  db2$year=year
+  db2$unit=unit
+  db2$sq=sq
+  db2$p16=p16
+  db2$palms=palms
+  db2$norm=FALSE # If the ground variables were normalized by area
   
-  dtotal=db
+  dtotal=db2
   return(dtotal)
   
   # if(write==TRUE){
